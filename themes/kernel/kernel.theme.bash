6279b71c27f7d61ce846b6f8d91e9861b2bfd00c
sanders-josh <jsanders@automationdirect.com>
2025-07-13 08:50:49 -0400

    enable fuck

--- themes/kernel/kernel.theme.bash
+++ themes/kernel/kernel.theme.bash
@@ -1,6 +1,8 @@
 # shellcheck shell=bash
 # shellcheck disable=SC2034 # Expected behavior for themes.
 
+bashit enable alias fuck
+
 SCM_THEME_PROMPT_DIRTY=" ${red?}✗${normal}"
 SCM_THEME_PROMPT_CLEAN=" ${bold_green?}✓${normal}"
 SCM_THEME_PROMPT_PREFIX=" ${blue}"
